041ce1f3292b44e365c0028904cb641721fdb1dd
alafroiskiotos <praetorian@akyroi.gr>
2012-04-21 23:32:27 +0300

    put some color

--- main.c
+++ main.c
@@ -4,14 +4,14 @@
 #include "proto.h"
 
 struct genes initialize (){
-    printf("Initializing...\n");
     struct genes tmp_gene;
-    
     int i, j;
 
-    for (i = 0; i < 11; i++){
-        for (j = 0; j < 7; j++){
+    for (i = 0; i < ROW; i++){
+        for (j = 0; j < COL; j++){
             long rand = random();
+            /* If even paint it white (0),
+                else paint it black (1) */
             if (rand % 2 == 0)
                 tmp_gene.gene[i][j] = 0;
             else
@@ -23,31 +23,51 @@ struct genes initialize (){
     return tmp_gene;
 }
 
-void comp_fitness(struct genes *tmp_gene){
-    printf("fitness: %d\n",tmp_gene.fitness);
+void comp_fitness(struct genes* tmp_gene){
+    int i, j, penalty = 0;
+
+    for (i = 0; i < ROW; i++){
+        for (j = 0; j < COL; j++){
+            /* Compare each cell to the original.
+                If there is a penalty increase by one
+                the counter */
+            if (tmp_gene->gene[i][j] != source[i][j])
+                penalty++;
+        }
+    }
+    /* Fitness is between 0 and 1 */
+    tmp_gene->fitness = (float) penalty/(COL * ROW);
 }
+
 void print_gene(struct genes tmp_gene){
     int i, j;
     for (i = 0; i < COL; i++){
         for (j = 0; j < ROW; j++){
-            printf("%d", tmp_gene.gene[i][j]);
+            if (tmp_gene.gene[i][j] == 1)
+                printf(GREEN "%2d" RESET, tmp_gene.gene[i][j]);
+            else
+                printf(WHITE "%2d" RESET, tmp_gene.gene[i][j]);
         }
         printf("\n");
     }
     printf("\n");
-    printf("fitness: %d\n", tmp_gene.fitness);
+    printf("fitness: %f\n", tmp_gene.fitness);
 }
 
 int main(int argc, char *argv[]){
-    
     struct genes total_genes[POPULATION];
     int i;
 
+    /* Initialize the first generation */
     for (i = 0; i < POPULATION; i++){
         total_genes[i] = initialize();
     }
 
+    
     for (i = 0; i < POPULATION; i++){
+        printf("====================\n");
+        /* Compute fitness */
+        comp_fitness(&total_genes[i]);
         print_gene(total_genes[i]);
     }
     exit(EXIT_SUCCESS);
--- proto.h
+++ proto.h
@@ -1,10 +1,13 @@
 #define COL 7
 #define ROW 11
 #define POPULATION 1000
+#define RESET "\033[0m"
+#define GREEN "\033[32m"      /* Green */
+#define WHITE "\033[37m"      /* White */
 
 struct genes{
     int gene[11][7];
-    int fitness;
+    float fitness;
 };
 
 static int source[11][7] = {
