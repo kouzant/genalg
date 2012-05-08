72bbe97602a377ac1824882110a757d8ff56f12d
alafroiskiotos <praetorian@akyroi.gr>
2012-05-08 17:10:03 +0300

    more documentation

--- src/color_graph.c
+++ src/color_graph.c
@@ -25,29 +25,6 @@
 
 #include "proto.h"
 
-/* Check whether an adjacent cell is colored */
-/* Eventually I don't need it */
-int check_adj(struct Organisms tmp_gene, int row, int col){
-    int no_adj = 1;
-
-    /* Check first row and all columns */
-    if (row == 0 && col >= 0){
-        if (tmp_gene.gene[row][col - 1] == 1)
-            no_adj = 0;
-        /* Check first column and all rows */
-    }else if (row >= 0 && col == 0){
-        if (tmp_gene.gene[row - 1][col])
-            no_adj = 0;
-        /* Check anything else in the grid */
-    }else if (row > 0 && col > 0){
-        if ((tmp_gene.gene[row][col - 1] == 1) ||
-                (tmp_gene.gene[row - 1][col] == 1))
-            no_adj = 0;
-    }
-
-    return no_adj;
-}
-
 /* Randomly initialize the Organisms */
 struct Organisms initialize (){
     struct Organisms tmp_gene;
@@ -106,21 +83,6 @@ void print_gene(struct Organisms tmp_gene){
     }
     printf("\n");
     printf("fitness: %f\n", tmp_gene.fitness);
-}
-
-/* Just want to see the source. Serves nothing */
-void print_source(){
-    int i, j;
-    for (i = 0; i < ROW; i++){
-        for (j = 0; j < COL; j++){
-            if (source[i][j] == 1)
-                printf(GREEN "%2d" RESET, source[i][j]);
-            else
-                printf(WHITE "%2d" RESET, source[i][j]);
-        }
-        printf("\n");
-    }
-    printf("\n");
 }
 
 /* Compute total fitness */
